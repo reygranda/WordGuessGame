869cd9769f21d13fc044f4674ebe6b8e3bc485e0
reygranda <rey030299@gmail.com>
2019-05-22 18:46:37 -0400

    committt

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,13 +6,37 @@ var guessesLeft = 5;
 var lettersGuessed = [];
 var computerGuess = [];
 
+var winsText = document.getElementById("");
+var lossesText = document.getElementById("");
+var guessesLeft = document.getElementById("");
+var guessesText = document.getElementById("");
+
+
 
 document.onkeyup = function (event) {
 	var userGuess = event.key;
 	var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-	
+
 	if (userGuess === computerGuess) {
 		wins++;
 		guessedLetters = [];
+		guesses = 10;
 	}
+
+else {
+	guesses--;
+	guessedLetters.push(userGuess);
 }
+
+if (guesses === 0) {
+	losses++;
+	guessedLetters = [];
+}
+if (guesses === 0) {
+	guesses = 10;
+}
+}
+winsText.textContent = "Wins: " + wins;
+losesText.textContent = "Losses: " + losses;
+guessesLeft.textContent = "Guesses Left: " + guesses;
+guessesText.textContent = "Your Guesses so far: " + guessedLetters;
